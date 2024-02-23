200ab9c52ba1890cc9c32a5825dc2d26f3e38a58
Freedom-21 <yenesewfreedom8@gmail.com>
2024-02-23 13:31:35 +0100

    fix execve

--- execute.c
+++ execute.c
@@ -6,40 +6,39 @@
  * Return: execute process on success or exit with error if not
  */
 
-void execute_cmd(char **argv) 
+void execute_cmd(char **argv)
 {
 	pid_t child_pid;
 	char *cmd_path;
-	
+
 	child_pid = fork();
-	
-	if (child_pid == 0) 
+
+	if (child_pid == 0)
 	{
 		cmd_path = find_cmd_in_path(argv[0]);
-		
-		if (cmd_path != NULL) 
+
+		if (cmd_path != NULL)
 		{
-			if (execve(cmd_path, argv, environ) == -1) 
+			if (execve(cmd_path, argv, environ) == -1)
 			{
 				perror("./shell");
 				free(cmd_path);
 			}
-			
+
 			exit(EXIT_FAILURE);
-        
-		} 
-		
-		else 
+		}
+
+		else
 		{
 			printf("%s: Command not found\n", argv[0]);
 			exit(EXIT_FAILURE);
 		}
-	} 
-	else if (child_pid > 0) 
+	}
+	else if (child_pid > 0)
 	{
 		wait(NULL);
 	}
-	else 
+	else
 	{
 		perror("fork");
 	}
