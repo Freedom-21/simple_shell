d9b6c81b6fd8021101b2f4515173ccd103bd29d9
Freedom-21 <yenesewfreedom8@gmail.com>
2024-02-28 11:53:23 +0100

    fix intractive mode

--- main.c
+++ main.c
@@ -12,31 +12,49 @@ int main(void)
 	ssize_t nread;
 	char **argv;
 
-	while (1)
+	if (isatty(STDIN_FILENO))
 	{
-		printf(":$ ");
-		nread = getline(&line, &len, stdin);
-
-		if (nread == -1)
-			break;
-
-		line[nread - 1] = '\0';
-		argv = parse_input(line);
-
-		if (argv == NULL)
-			continue;
-
-		if (_strcmp(argv[0], "exit") == 0)
-			exit(0);
-
-		if (_strcmp(argv[0], "env") == 0)
+		while (1)
 		{
-			print_env();
+			printf(":$ ");
+			nread = getline(&line, &len, stdin);
+
+			if (nread == -1)
+				break;
+
+			line[nread - 1] = '\0';
+			argv = parse_input(line);
+
+			if (argv == NULL)
+				continue;
+
+			if (_strcmp(argv[0], "exit") == 0)
+			{
+				free(argv);
+				break;
+			}
+			if (_strcmp(argv[0], "env") == 0)
+			{
+				print_env();
+				free(argv);
+				continue;
+			}
+			execute_cmd(argv);
 			free(argv);
-			continue;
 		}
-		execute_cmd(argv);
-		free(argv);
+	}
+	else
+	{
+		while	((nread = getline(&line, &len, stdin)) != -1)
+		{
+			line[nread - 1] = '\0';
+			argv = parse_input(line);
+			if (argv != NULL)
+			{
+				execute_cmd(argv);
+				free(argv);
+			}
+		}
 	}
 	free(line);
 	return (0);
